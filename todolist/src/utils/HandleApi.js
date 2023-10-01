0bf2076275ec1079382a36c07207b87d83ad65d5
Shivesh Nandan <kshivesh929@gmail.com>
2023-10-02 01:20:41 +0530

    complete

--- todolist/src/utils/HandleApi.js
+++ todolist/src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://todo-with-backend-lyart.vercel.app"
+const baseURL = "todo-with-backend-tau.vercel.app"
 
 const getAllToDo = async (setToDo) => {
     axios
